b60614d2ecd02e7d498eadba04c367a9364e18f3
Alessandro Facchin <alessandro.facchin@outlook.com>
2020-05-25 08:35:04 +0100

    small change

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32).hex()
+    SECRET_KEY = os.environ.get('BRISCOLA_SECRET_KEY') or os.urandom(32).hex()
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
